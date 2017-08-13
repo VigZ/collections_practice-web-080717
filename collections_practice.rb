9ca363bddcda44a2e4b396c8871737299562174e
github username <github email address>
2017-08-13 03:20:05 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|element, element2| element2 <=> element}
+end
+
+def sort_array_char_count(array)
+  array.sort_by{|element| element.length }
+end
+
+def swap_elements_from_to(array, index, destination)
+  array[index], array[destination] = array[destination], array[index]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array_of_strings)
+  array_of_strings.collect do |string|
+  string[2] = "$"
+  string
+  end
+end
+
+def find_a(array)
+  array.select{ |element| element.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject{|sum,element| sum + element}
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+     if index != 1
+       element + "s"
+     else
+       element
+     end
+   end
+end
--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -24,9 +24,9 @@ describe 'collections practice' do
   end
 
   # Question 4
-  describe '#swap_elements' do
-    it 'swap the second and third elements of an array' do
-      expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
+  describe '#swap_elements_from_to' do
+    it 'swap elements from inital location to target location' do
+      expect(swap_elements_from_to(["blake", "ashley", "scott"],1,2)).to eq(["blake", "scott", "ashley"])
     end
   end
 
